22593f9a2ca7667ae2370e9ae97d7115ddbab06d
YoungdonJung <youngdon.jung@lotte.net>
2023-09-07 16:22:43 +0900

    Add blog

---
+++ VueDjAgency/blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Post(models.Model):
+    pass
+
+class Category(models.Model):
+    pass
+
+class Tag(models.Model):
+    pass
+
+class Commnet(models.Model):
+    pass
