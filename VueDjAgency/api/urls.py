5bde2c6669a4e45fbdbfea7d44b32d85deeb1b47
YoungdonJung <youngdon.jung@lotte.net>
2023-09-19 16:57:08 +0900

    Like

--- VueDjAgency/api/urls.py
+++ VueDjAgency/api/urls.py
@@ -8,5 +8,6 @@ app_name = 'api'
 urlpatterns = [
     path('post/list/', views.ApiPostLV.as_view(), name='post_list'),
     path('post/<int:pk>/', views.ApiPostDV.as_view(), name='post_detail'),
-    path('catetag/', views.ApiCateTagView.as_view(), name='catetag_list')
+    path('catetag/', views.ApiCateTagView.as_view(), name='catetag_list'),
+    path('like/<int:pk>/', views.ApiPostLikeDV.as_view(), name='post_like')
 ]
--- VueDjAgency/api/views.py
+++ VueDjAgency/api/views.py
@@ -53,4 +53,12 @@ class ApiCateTagView(View):
             'tagList': tagList
         }
         return JsonResponse(data=jsonData, safe=True, status=200)
-            
+
+class ApiPostLikeDV(BaseDetailView):
+    model = Post
+    
+    def render_to_response(self, context, **response_kwargs):
+        obj = context['object']
+        obj.like += 1
+        obj.save()
+        return JsonResponse(data=obj.like, safe=False, status=200)
