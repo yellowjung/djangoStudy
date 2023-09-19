f682537b1207a3a144800c6c44a554b3d73db4c8
YoungdonJung <youngdon.jung@lotte.net>
2023-09-19 13:59:38 +0900

    Prev Post, Next Post

--- VueDjAgency/api/utils.py
+++ VueDjAgency/api/utils.py
@@ -1,8 +1,10 @@
-def obj_to_post(obj):
+def obj_to_post(obj, flag=True):
     """obj의 각 속성을 serialize 해서, dict 로 변환한다
-
-    Args:
-        obj (_type_): _description_
+    
+    serialize: python object --> (기본 타입) int, float, str
+    :param obj:
+    :param flag: True (모두 보냄, /api/post/99/ 용), false (일부 보냄, /api/post/list/ 용)
+    :return:
     """
     
     post = dict(vars(obj))
@@ -20,13 +22,38 @@ def obj_to_post(obj):
     if obj.image:
         post['image'] = obj.image.url
     else:
-        post['image'] = 'https://via.placeholder.com/900x400/'
+        post['image'] = 'https://via.placeholder.com/900x300/'
         
     if obj.update_dt:
         post['update_dt'] = obj.update_dt.strftime('%Y-%m-%d %H:%M:%s')
     else:
         post['update_dt'] = '9999-12-31 00:00:00'
     
-    del post['_state'], post['category'], post['create_dt']
+    del post['_state'], post['category_id'], post['create_dt']
+    
+    if not flag:
+        del post['tags'], post['update_dt'], post['description'], post['content']
     
     return post
+
+def prev_next_post(obj):
+    try:
+        prevObj = obj.get_previous_by_update_dt()
+        prevDict = {
+            'id' : prevObj.id,
+            'title' : prevObj.title
+        }
+    except obj.DoesNotExist:
+        prevdict = {}
+        
+
+    try:
+        nextObj = obj.get_next_by_update_dt()
+        nextDict = {
+            'id' : nextObj.id,
+            'title' : nextObj.title
+        }
+    except obj.DoesNotExist:
+        nextDict = {}
+        
+    return prevDict, nextDict
--- VueDjAgency/api/views.py
+++ VueDjAgency/api/views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views.generic.list import BaseListView
-from api.utils import obj_to_post
+from django.views.generic.detail import BaseDetailView
+from api.utils import obj_to_post, prev_next_post
 
 from blog.models import Post
 
@@ -9,6 +10,21 @@ class ApiPostLV(BaseListView):
     
     def render_to_response(self, context, **response_kwargs):
         qs = context['object_list']
-        postList = [obj_to_post(obj) for obj in qs]
+        postList = [obj_to_post(obj, False) for obj in qs]
         
         return JsonResponse(data = postList, safe = False, status = 200)
+    
+class ApiPostDV(BaseDetailView):
+    model = Post
+    
+    def render_to_response(self, context, **response_kwargs):
+        obj = context['object']
+        post = obj_to_post(obj)
+        prevPost, nextPost = prev_next_post(obj)
+        
+        jsonData = {
+            'post' : post,
+            'prevPost' : prevPost,
+            'nextPost' : nextPost
+        }
+        return JsonResponse(data = jsonData, safe = True, status = 200)
