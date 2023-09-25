45ed61405d2639f244036c2cf8ef194ac6236852
YoungdonJung <youngdon.jung@lotte.net>
2023-09-25 15:36:14 +0900

    csrf, v-cloak

--- VueDjAgency/blog/views.py
+++ VueDjAgency/blog/views.py
@@ -1,8 +1,40 @@
-from django.views.generic import DetailView
+import json
+from django.views.generic import DetailView, TemplateView
+from api.utils import obj_to_comment, obj_to_post, prev_next_post
 
-from blog.models import Post
+from blog.models import Category, Post, Tag
 
 # Create your views here.
-class PostDV(DetailView): 
+# class PostDV(TemplateView): 
+#     template_name = 'blog/post_detail.html'
+
+class PostDV(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().getContext()
+        
+        obj = context['object']
+        post = obj_to_post(obj)
+        prevPost, nextPost = prev_next_post(obj)
+        
+        qsComment = obj.comment_set.all()
+        commentList = [obj_to_comment(obj)for obj in qsComment]
+        
+        qs1 = Category.objects.all()
+        qs2 = Tag.objects.all()
+        cateList = [cate.name for cate in qs1]
+        tagList = [tag.name for tag in qs2]
+        
+        dataDict={
+            'post' : post,
+            'prevPost' : prevPost,
+            'nextPost' : nextPost,
+            'commentList' : commentList,
+            'cateList' : cateList,
+            'tagList' : tagList
+        }
+        
+        context['myJson'] = json.dumps(dataDict)
+        return context
